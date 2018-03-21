5dabe9bd0cabc102efc8eb66d41afb912ab1aba3
estrattonbailey <ericstrattonbailey@gmail.com>
2018-03-21 17:06:29 -0400

    .

---
+++ README.md
@@ -0,0 +1,58 @@
+# picoapp
+Teeny tiny components. Teeny tiny hassle. Teeny tiny size: **450 bytes
+gzipped.**
+
+## Install
+```
+npm i picoapp
+```
+
+# Usage
+```javascript
+import app from 'picoapp'
+
+function Counter (node) {
+  let count = 0
+
+  function render () {
+    node.innerHTML = count
+  }
+
+  return {
+    inc () {
+      count++
+      render()
+    },
+    mount () {
+      render()
+    }
+  }
+}
+
+function IncrementCount (el) {
+  const node = document.querySelector(el.getAttribute('data-target'))
+  const counter = app.get(node)
+
+  el.addEventListener('click', e => {
+    counter.inc()
+  })
+}
+
+app.register({
+  counter: Counter,
+  inc: IncrementCount
+})
+
+app.mount('component', 'helper')
+```
+```html
+...
+  <div>
+    <div id='counter' data-component='counter'></div>
+    <button data-helper='inc' data-target='#counter'>Increment</button>
+  </div>
+...
+```
+
+## License
+MIT License © [Eric Bailey](https://estrattonbailey.com)
---
+++ index.js
@@ -0,0 +1,67 @@
+const cache = new Map()
+
+const components = {}
+
+export function add (...args) {
+  while (args.length) {
+    Object.assign(components, args.pop())
+  }
+}
+
+export function mount (...types) {
+  let nodes
+
+  for (let i = 0; i < types.length; i++) {
+    const type = types[i]
+    const attr = 'data-' + type
+    nodes = [].slice.call(document.querySelectorAll('[' + attr + ']'))
+
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i]
+      const name = node.getAttribute(attr)
+      const component = components[name]
+      if (component) {
+        const instance = component(node)
+        cache.set(node, instance)
+        node.removeAttribute(attr)
+        if (instance.mount && typeof instance.mount === 'function') {
+          instance.mount()
+        }
+      }
+    }
+  }
+
+  return nodes
+}
+
+export function get (node) {
+  return cache.get(node)
+}
+
+export function unmount (nodes) {
+  if (nodes) {
+    while (nodes.length) {
+      cache.delete(nodes.pop())
+    }
+  } else {
+    let unmounts = []
+
+    cache.forEach((instance, key) => {
+      if (instance.unmount) {
+        if (typeof instance.unmount === 'function') {
+          unmounts.push(instance.unmount())
+        }
+        cache.delete(key)
+      }
+    })
+
+    return Promise.all(unmounts)
+  }
+}
+
+export default {
+  register,
+  mount,
+  get,
+  unmount
+}
