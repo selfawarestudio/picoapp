3afaa9845dfb5a51c7eb898fe8645776928a97cc
estrattonbailey <ericstrattonbailey@gmail.com>
2019-04-03 09:58:21 -0400

    fix bad ref

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ export function picoapp (components = {}, initialState = {}) {
         [ev]: data
       })
 
-      emit(ev, (state = s))
+      emit(ev, state)
     },
     get state () {
       return state
